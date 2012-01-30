76e0fe0daca76bd7760f6f125a98e7f2b89f56a3
EECOLOR <EECOLOR@eb74b4c4-f2d1-ffc1-b60c-1b5dace91c67>
2012-01-30 19:18:34 +0000

    Added owner to Metadata

--- ImplicitMetadata/resource/TestImplicitMetadataPlugin.scala
+++ ImplicitMetadata/resource/TestImplicitMetadataPlugin.scala
@@ -3,14 +3,19 @@ object TestImplicitMetadataPlugin {
     
     val val_a_string = ""
     val val_b_int = 0
-    val val_c_metadata:Metadata = new Metadata("not implicit Metadata", classOf[Metadata])
+    val val_c_metadata:Metadata = new Metadata("not implicit Metadata", classOf[Metadata], null)
     val val_d_list_string = List[String]()
+    
+    val subInstance = new {
+        val sub_element_other_owner = ""
+    }
     
 	def main(args: Array[String]) = {
 	    test(val_a_string);
 	    test(val_b_int)
 	    test(val_c_metadata)
 	    test(val_d_list_string)
+	    test(subInstance.sub_element_other_owner)
 	}
 
     def test(metadata:Metadata) = {
